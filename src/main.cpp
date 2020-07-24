432ff36223893c7b6082c268a0cdcf3d5b4ef4f0
Lars Englund <lars.englund@gmail.com>
2020-07-24 23:10:14 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,8 @@ const int thermocouple_so = D6;
 const int thermocouple_cs = D0;
 const int thermocouple_sck = D8;
 const int max_num_segments = 10;
+const int minutes_per_pixel = 3;
+const int degrees_per_pixel = 50;
 
 MAX31855soft myMAX31855(thermocouple_cs, thermocouple_so, thermocouple_sck);
 
@@ -44,8 +46,10 @@ float thermocouple_temp = 0;
 uint32_t tft_refresh_timestamp = 0;
 int num_segments = 5;
 int current_segment = 2;
-int segment_rate[max_num_segments] = {80, 100, 50, 80, 0, 0, 0, 0, 0, 0};
-int segment_target[max_num_segments] = {250, 800, 1000, 1200, 0, 0, 0, 0, 0, 0};
+File file_current_state;
+int segment_rate[max_num_segments] = {100, 200, 300, 400, 0, 0, 0, 0, 0, 0};
+int segment_target[max_num_segments] = {200, 300, 900, 1200, 0, 0, 0, 0, 0, 0};
+char profile_name[32] = "test profile";
 
 // ST7735 TFT module connections
 #define TFT_RST   D4     // TFT RST pin is connected to NodeMCU pin D4 (GPIO2)
@@ -141,9 +145,10 @@ void onPageNotFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
-void readThermocouple() {
+void readThermocouple(bool debug = false) {
   while (myMAX31855.detectThermocouple() != MAX31855_THERMOCOUPLE_OK)
   {
+    if (!debug) break;
     switch (myMAX31855.detectThermocouple())
     {
       case MAX31855_THERMOCOUPLE_SHORT_TO_VCC:
@@ -170,19 +175,59 @@ void readThermocouple() {
   }
 
   rawData = myMAX31855.readRawData();
-
-  Serial.print(F("Chip ID: "));
-  Serial.println(myMAX31855.getChipID(rawData)); //if ChipID != 31855, then you have read fail=2 or unknown error=2000
-
-  Serial.print(F("Cold Junction: "));
-  Serial.println(myMAX31855.getColdJunctionTemperature(rawData));
-
-  Serial.print(F("Thermocouple: "));
   thermocouple_temp = myMAX31855.getTemperature(rawData);
-  Serial.println(thermocouple_temp);
+
+  if (debug) {
+    Serial.print(F("Chip ID: "));
+    Serial.println(myMAX31855.getChipID(rawData)); //if ChipID != 31855, then you have read fail=2 or unknown error=2000
+
+    Serial.print(F("Cold Junction: "));
+    Serial.println(myMAX31855.getColdJunctionTemperature(rawData));
+
+    Serial.print(F("Thermocouple: "));
+    Serial.println(thermocouple_temp);
+  }
 }
 
-void setSegmentColor(int n);
+void setSegmentColor(int n) {
+  if (n == current_segment) {
+    tft.setTextColor(TFT_GREEN);
+  }
+  else if (n < current_segment) {
+    tft.setTextColor(TFT_DARKGREEN);
+  }
+  else if (n > num_segments) {
+    tft.setTextColor(TFT_NAVY);
+  }
+  else {
+    tft.setTextColor(TFT_DARKGREY);
+  }
+}
+
+void drawProfileGraph() {
+  int prev_segment_target = 0;
+  int segment_time;
+  int prev_x = 0;
+  int prev_y = 0;
+  int new_x, new_y;
+  for (int n=0; n<num_segments; n++) {
+    if (segment_rate[n] != 0) {
+      segment_time = abs(60 * (segment_target[n] - prev_segment_target)/segment_rate[n]);
+    }
+    else {
+      segment_time = 0;
+    }
+    new_x = prev_x + segment_time/minutes_per_pixel;
+    new_y = segment_target[n]/degrees_per_pixel;
+    if (n%2 == 1) {}
+    tft.drawLine(prev_x, 127-prev_y, new_x, 127-new_y, (n%2 == 1 ? TFT_MAGENTA : TFT_PURPLE));
+    prev_x = new_x;
+    prev_y = new_y;
+    prev_segment_target = segment_target[n];
+  }
+}
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -194,6 +239,7 @@ void setup() {
   //tft.setRotation(2);
   tft.fillScreen(ST7735_BLACK);
   tft.setCursor(0, 0);
+  tft.setTextWrap(false);
   //tft.setFont(&Picopixel);
   tft.setTextColor(ST7735_WHITE);
   tft.println("Golem setup...");
@@ -207,18 +253,43 @@ void setup() {
     delay(5000);
   }
   Serial.println(F("MAX31855 OK"));
+  readThermocouple(true);
 
   // Make sure we can read the file system
   if( !SPIFFS.begin()){
     Serial.println("Error mounting SPIFFS");
     while(1);
   }
+  Serial.println("Listing firing profiles:");
+  Dir dir = SPIFFS.openDir("/prog");
+  int n=0;
+  while (dir.next()) {
+    Serial.print("  ");
+    Serial.print(dir.fileName());
+    Serial.print(" ");
+    if(dir.fileSize()) {
+        File f = dir.openFile("r");
+        Serial.println(f.size());
+        f.close();
+        n++;
+    }
+  }
+  Serial.printf("Found %d firing profiles\n", n);
+  file_current_state = SPIFFS.open("/state/current", "a+");
+  if (file_current_state.size() == 0) {
+    Serial.println("No current state stored");
+  }
+  else {
+    char buffer[32];
+    int len = file_current_state.readBytesUntil('\n', buffer, sizeof(buffer)-1);
+    buffer[len] = '\0';
+    Serial.printf("Last profile was %s", buffer);
+  }
 
   // Start access point
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   if (WiFi.softAP(ssid, password)) {
-    Serial.println();
     Serial.println("AP started");
     Serial.print("SSID: ");
     Serial.println(ssid);
@@ -281,8 +352,7 @@ void loop() {
     tft.setCursor(0, 0);
     //tft.setFont(&Picopixel);
     tft.setTextColor(ST7735_WHITE);
-    tft.setTextWrap(true);
-    tft.println("KILN CELSIUS");
+    //tft.println("KILN CELSIUS");
 
     //tft.setCursor(0, 45);
     //tft.setFont(&FreeSansBold24pt7b);
@@ -292,54 +362,53 @@ void loop() {
     //tft.println("1337");
     tft.setTextSize(1);
 
-    tft.drawLine(72,0,72,30,ST7735_GREEN);
+    tft.drawLine(72,0,72,32,ST7735_GREEN);
 
     tft.setCursor(74, 0);
     tft.setTextColor(ST7735_RED);
     tft.print("WIFI ");
     tft.setTextColor(ST7735_GREEN);
     tft.print("HEAT");
-    tft.setCursor(74, 14);
+    tft.setCursor(74, 8);
+    tft.setTextColor(ST7735_GREEN);
+    tft.print("PROG");
+    tft.setCursor(74, 16);
     tft.printf("%4.1f/%4.1f", 1.2, 2.5);
-    tft.setCursor(74, 22);
+    tft.setCursor(74, 24);
     tft.printf("%4.1f/%4.1f", 2.2, 4.5);
 
-    tft.drawLine(0,30,128,30,ST7735_GREEN);
-    tft.drawLine(0,30,128,30,ST7735_GREEN);
+    tft.drawLine(0,32,128,32,ST7735_GREEN);
 
-    tft.setCursor(0, 32);
+    tft.setCursor(0, 34);
+    tft.print(profile_name);
+    tft.setCursor(0, 42);
     tft.setTextColor(ST7735_GREEN);
     //tft.setFont(&Picopixel);
     for (int n=1; n<max_num_segments; n+=2) {
       setSegmentColor(n);
       tft.printf("%2d", n);
-      tft.printf("%4d", segment_rate[n]);
-      tft.printf("%4d ", segment_target[n]);
+      if (n == current_segment) {
+        tft.setTextColor(TFT_WHITE);
+      }
+      tft.printf("%4d", segment_rate[n-1]);
+      setSegmentColor(n);
+      tft.printf("%4d ", segment_target[n-1]);
       setSegmentColor(n+1);
       tft.printf("%2d", n+1);
-      tft.printf("%4d", segment_rate[n+1]);
-      tft.printf("%4d\n", segment_target[n+1]);
+      if (n+1 == current_segment) {
+        tft.setTextColor(TFT_WHITE);
+      }
+      tft.printf("%4d", segment_rate[n]);
+      setSegmentColor(n+1);
+      tft.printf("%4d\n", segment_target[n]);
     }
 
-    tft.drawLine(0,80,128,80,ST7735_GREEN);
+    tft.drawLine(0,82,128,82,ST7735_GREEN);
+
+    drawProfileGraph();
   }
 
   dnsServer.processNextRequest();
   webSocket.loop();
   delay(10);
-}
-
-void setSegmentColor(int n) {
-  if (n == current_segment) {
-    tft.setTextColor(ST7735_RED);
-  }
-  else if (n < current_segment) {
-    tft.setTextColor(ST7735_GREEN);
-  }
-  else if (n > num_segments) {
-    tft.setTextColor(ST7735_BLUE);
-  }
-  else {
-    tft.setTextColor(ST7735_YELLOW);
-  }
 }
